cfa13254ff56df5ca995eab1e223df76c3aeb694
Theaummiez <tomybout@gmail.com>
2026-05-30 12:17:36 +0700

    Fix bug opfully last time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import { LoginGate } from "@/components/login-gate";
 import { Nav } from "@/components/nav";
 import { Hero } from "@/components/hero";
@@ -10,13 +13,14 @@ import { LoveLetter } from "@/components/love-letter";
 import { BucketList } from "@/components/bucket-list";
 import { Countdown } from "@/components/countdown";
 import { Footer } from "@/components/footer";
-import { Particles } from "@/components/particles";
-import { Fireworks } from "@/components/fireworks";
-import { ClickHearts } from "@/components/click-hearts";
-import { ScrollRose } from "@/components/scroll-rose";
 import { ScrollProgress } from "@/components/scroll-progress";
-import { HeartCursor } from "@/components/heart-cursor";
 import { EasterEgg } from "@/components/easter-egg";
+
+const Particles = dynamic(() => import("@/components/particles").then(mod => ({ default: mod.Particles })), { ssr: false });
+const Fireworks = dynamic(() => import("@/components/fireworks").then(mod => ({ default: mod.Fireworks })), { ssr: false });
+const ClickHearts = dynamic(() => import("@/components/click-hearts").then(mod => ({ default: mod.ClickHearts })), { ssr: false });
+const ScrollRose = dynamic(() => import("@/components/scroll-rose").then(mod => ({ default: mod.ScrollRose })), { ssr: false });
+const HeartCursor = dynamic(() => import("@/components/heart-cursor").then(mod => ({ default: mod.HeartCursor })), { ssr: false });
 
 export default function Home() {
   return (
